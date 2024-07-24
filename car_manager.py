d4e716fba2e6cc6debd3ada3f3b3f45f1b28ffca
Claudiu357 <neculaclaudiu21@gmail.com>
2024-07-24 17:49:07 +0300

    i implemented the cars and player logic

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,35 @@
+import random
+import time
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+CHANCE = [0, 0, 0, 0, 0, 1]
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.last_y = None
+        self.hideturtle()
+        self.all_cars = []
+        self.Y_POS = []
+        for _ in range(-280, 281, 20):
+            self.Y_POS.append(_)
+
+    def add_car(self):
+        t = Turtle()
+        t.shape("square")
+        t.penup()
+        t.shapesize(stretch_len=2, stretch_wid=1)
+        t.color(random.choice(COLORS))
+        t.setheading(180)
+        random_y = random.choice(self.Y_POS)
+        t.goto(x=300, y=random_y)
+        self.all_cars.append(t)
+
+    def move_cars(self):
+        if random.choice(CHANCE) == 1:
+            self.add_car()
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
